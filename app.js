101269d84c33dab376319ed85b94b412f37ee0b0
Hayden <hayden@aitch.co>
2020-01-03 19:35:19 +1000

    split header and footer into seperate blocks for easier templating

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ let sessionOptions = session({
 app.use(sessionOptions);
 app.use(flash());
 
+app.use(function(req, res, next) {
+    res.locals.user = req.session.user
+    next()
+})
+
 const router = require('./router');
 
 // Allow express to recieve form data and json data
