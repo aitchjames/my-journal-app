e97941bcdce0bb361daa3384d8abc48c82992515
Hayden <hayden@aitch.co>
2020-01-09 18:38:30 +1000

    responding to key press events for the search feature and added axios

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default class Search {
     // Selecting DOM elements in constructor and keep track of useful data
     constructor() {
@@ -5,21 +7,54 @@ export default class Search {
         this.headerSearchIcon = document.querySelector(".header-search-icon");
         this.overlay = document.querySelector(".search-overlay");
         this.closeIcon = document.querySelector(".close-live-search");
+        this.inputField = document.querySelector("#live-search-field");
+        this.resultsArea = document.querySelector(".live-search-results");
+        this.loaderIcon = document.querySelector(".circle-loader");
+        this.typingWaitTimer;
+        this.previousValue = "";
         this.events();
     }
 
     // Events
     events() {
+        this.inputField.addEventListener("keyup", () => this.keyPressHandler())
         this.closeIcon.addEventListener("click", () => this.closeOverlay())
         this.headerSearchIcon.addEventListener("click", (event) => {
             event.preventDefault()
             this.openOverlay()
         })
     }
-
+    //live-search-results--visible
     // Methods
+    keyPressHandler() {
+        let value = this.inputField.value;
+
+        if (value != "" && value != this.previousValue) {
+            clearTimeout(this.typingWaitTimer);
+            this.showLoaderIcon();
+            this.typingWaitTimer = setTimeout(() => this.sendRequest() , 1500);
+        }
+
+        this.previousValue = value;
+    }
+
+    sendRequest() {
+        axios.post('/search', {
+            searchTerm: this.inputField.value
+        }).then(() => {
+
+        }).catch(() => {
+            alert("Failed, but atleast this function works right?")
+        })
+    }
+
+    showLoaderIcon() {
+        this.loaderIcon.classList.add("circle-loader--visible");
+    }
+
     openOverlay() {
         this.overlay.classList.add("search-overlay--visible");
+        setTimeout(() => this.inputField.focus() , 50);
     }
 
     closeOverlay() {
@@ -39,7 +74,7 @@ export default class Search {
         <div class="search-overlay-bottom">
           <div class="container container--narrow py-3">
             <div class="circle-loader"></div>
-            <div class="live-search-results live-search-results--visible">
+            <div class="live-search-results">
               <div class="list-group shadow-sm">
                 <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
     
