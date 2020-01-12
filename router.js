574649b83c8e2f9c7aa763abdec82007e99936e7
Hayden <hayden@aitch.co>
2020-01-12 13:29:02 +1000

    users can now follow each other and updates counts on profile screens

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./controllers/userController");
 const postController = require("./controllers/postController");
+const followController = require("./controllers/followController");
 
 // User Routes
 router.get('/', userController.home);
@@ -10,7 +11,9 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 
 // Profile Routes
-router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen);
+router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen);
+router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen);
+router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen);
 
 // Post Routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen);
@@ -23,5 +26,8 @@ router.post('/post/:id/delete', userController.mustBeLoggedIn, postController.de
 // Search Route
 router.post('/search', postController.search);
 
+// Follow Routes
+router.post('/addFollow/:username', userController.mustBeLoggedIn, followController.addFollow);
+router.post('/removeFollow/:username', userController.mustBeLoggedIn, followController.removeFollow);
 
 module.exports = router;
